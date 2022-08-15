5ba0c274253a928f6037ac1b259c8fef9b6677f5
KevinNewen <kevin.nguyen1996@gmail.com>
2022-08-15 22:15:17 +0000

    Migrate AppSlice to React Query

--- src/views/V1-Stake/__tests__/V1-Stake.unit.test.tsx
+++ src/views/V1-Stake/__tests__/V1-Stake.unit.test.tsx
@@ -1,7 +1,6 @@
 import { connectWallet } from "src/testHelpers";
-
-import { render } from "../../../testUtils";
-import V1Stake from "../V1-Stake";
+import { render } from "src/testUtils";
+import V1Stake from "src/views/V1-Stake/V1-Stake";
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -11,11 +10,13 @@ afterEach(() => {
 describe("<Stake/>", () => {
   it("should render component. not connected", async () => {
     const { container } = render(<V1Stake setMigrationModalOpen={false} />);
+
     expect(container).toMatchSnapshot();
   });
 
   it("should render the stake input Area when connected", async () => {
     connectWallet();
+
     const { container } = render(<V1Stake setMigrationModalOpen={false} />);
     expect(container).toMatchSnapshot();
   });
